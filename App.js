ad985619104e5e11ba1da4f3a029557ea69d0daf
ivan3455 <92949894+ivan3455@users.noreply.github.com>
2024-01-17 20:32:32 +0200

    labs 1-2 done

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, SafeAreaView, StatusBar, Alert, TouchableOpacity, View, Button } from "react-native";
 
 export default function App() {
+  const handleTextPress = () => Alert.alert("Alert window");
+
+  const handleButtonPress = () => Alert.alert(
+    'Simple ALert',
+      'Alert text',
+      [
+        { text: 'Ask me later', onPress: () => console.log('Ask me later pressed') },
+        {
+          text: 'Cancel',
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'cancel',
+        },
+        { text: 'OK', onPress: () => console.log('OK Pressed') },
+      ], { cancelable: false }
+  );
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <TouchableOpacity onPress={handleTextPress} style={styles.textContainer}>
+        <View>
+          <Text numberOfLines={1} style={styles.text}>
+            Text
+          </Text>
+        </View>
+      </TouchableOpacity>
       <StatusBar style="auto" />
-    </View>
+      <View style={styles.buttonContainer}>
+        <Button
+          onPress={handleButtonPress}
+          title="Button"
+          style={styles.button}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textContainer: {
+    borderWidth: 1,
+    borderColor: "black",
+    backgroundColor: "green",
+  },
+  buttonContainer: {
+    paddingTop: 10,
+  },
+  text: {
+    padding: 60,
+    color: "yellow",
   },
 });
